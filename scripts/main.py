d0f8b379741c86eeb7633378abaedc8d3548df67
Nikita <nikita29042001@yandex.ru>
2022-01-16 11:56:27 +0300

    Import updates

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import gripper_pkg
+import gripper_pkg.grip_serv as gp_pk
 
 if __name__ == "__main__":
-    gp_sv = gripper_pkg.GripperService()
+    gp_sv = gp_pk.GripperService()
     gp_sv.handle_control_message_server()
