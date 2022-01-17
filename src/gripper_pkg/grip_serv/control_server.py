99f215a882cc975dc059d491176033b6c1c5326a
Nikita Popov <nikita29042001@yandex.ru>
2022-01-17 12:17:30 +0300

    Test control functions realised

--- src/gripper_pkg/grip_serv/control_server.py
+++ src/gripper_pkg/grip_serv/control_server.py
@@ -9,17 +9,26 @@ import rospy
 
 
 class GripperService:
+    
+    def open(self):
+        self.gripper.open()
+    
+    def close(self):
+        self.gripper.close()
 
     def __init__(self):
-        pass
+        self.gripper = GripperSerialController('/dev/ttyACM0', 57600)
+        self.pseudo_switch = {"110":self.open, "111":self.close}
 
-    def handle_control_message(self, message):
+    def handle_control_message(self, request):
         log_string = "Message recieved at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S")
         rospy.loginfo(log_string)
-        print("Message")
-        print(message.operation_type)
-        print(message.speed)
-        print(message.position)
+        print("Request")
+        print(request.operation_type)
+        print(request.speed)
+        print(request.position)
+        self.pseudo_switch['%d'%request.operation_type]()
+        return "Operation %d ended"%request.operation_type
 
     def handle_control_message_server(self):
         rospy.init_node('gripper_control_node')
@@ -27,6 +36,6 @@ class GripperService:
         log_string = "service started at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S")
         rospy.loginfo(log_string)
         rospy.spin()
-
+        
 if __name__ == "__main__":
     handle_control_message_server()
