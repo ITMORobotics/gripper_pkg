cbddaefc89e156e7efa47a05a8ff5ecc86a99c74
Nikita Popov <nikita29042001@yandex.ru>
2022-01-17 15:13:00 +0300

    Functions and logger update

--- src/gripper_pkg/grip_serv/control_server.py
+++ src/gripper_pkg/grip_serv/control_server.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3 
 from __future__ import print_function
 
-from .gripper_controller import GripperSerialController
 import datetime
-import logging
-from gripper_pkg.srv import control
 import rospy
+from .gripper_controller import GripperSerialController
+from gripper_pkg.srv import control
 
 
 class GripperService:
     
-    def open(self):
-        self.gripper.open()
-    
-    def close(self):
-        self.gripper.close()
-
     def __init__(self):
-        self.gripper = GripperSerialController('/dev/ttyACM0', 57600)
-        self.pseudo_switch = {"110":self.open, "111":self.close}
+        try:
+            self.gripper = GripperSerialController('/dev/ttyACM0', 57600)
+        except:
+            print("COM port unavailable")
+        self.pseudo_switch = {"100":self.release, "101":self.unrelease, "110":self.open, "111":self.close}
 
     def handle_control_message(self, request):
         log_string = "Message recieved at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S")
         rospy.loginfo(log_string)
         print("Request")
-        print(request.operation_type)
-        print(request.speed)
-        print(request.position)
-        self.pseudo_switch['%d'%request.operation_type]()
+        print("Operation type: %d"%request.operation_type)
+        print("Speed: %d"%request.speed)
+        print("Position %d"%request.position)
+        try:
+            self.pseudo_switch['%d'%request.operation_type]()
+        except:
+            rospy.loginfo("While operation error occured %s"%datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S"))
+                    
         return "Operation %d ended"%request.operation_type
 
     def handle_control_message_server(self):
@@ -36,6 +36,15 @@ class GripperService:
         log_string = "service started at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S")
         rospy.loginfo(log_string)
         rospy.spin()
-        
-if __name__ == "__main__":
-    handle_control_message_server()
+    
+    def open(self):
+        self.gripper.open()
+
+    def close(self):
+        self.gripper.close()    
+    
+    def release():
+        self.gripper.release()
+
+    def unrelease():
+        self.gripper.unrelease()
