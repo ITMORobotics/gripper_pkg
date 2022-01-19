cd5ab45b71c5181bf45350b17106bff7de1caa87
Nikita Popov <nikita29042001@yandex.ru>
2022-01-19 11:57:18 +0300

    Logs fix

--- src/gripper_pkg/grip_serv/control_server.py
+++ src/gripper_pkg/grip_serv/control_server.py
@@ -14,7 +14,7 @@ class GripperPublisher(GripperListenerI):
         self.publisher = rospy.Publisher('from_gripper_info', String, queue_size=10)
 
     def process_data(self, package: bytes, type_code:int, left_val: float, right_val: float)->None:
-        pub_str = "time: %s     package: %s    type_code:%d     left_val:%f     right_val:%f"%(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S"), package.hex(":"), type_code, left_val, right_val)
+        pub_str = "Time: %s | package: %s | type_code: %d | left_val: %f | right_val: %f"%(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), package.hex(":"), type_code, left_val, right_val)
         rospy.loginfo(pub_str)
         self.publisher.publish(pub_str)
 
@@ -27,27 +27,25 @@ class GripperService:
             self.gripper.attach(listener=GripperPublisher())
             self.gripper.start_listening()
         except:
-            rospy.loginfo("COM port connection error  %s"%datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S"))
+            rospy.loginfo("COM port connection error  %s"%datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         self.pseudo_switch = {"10":self.get_position,"20":self.get_load,"30":self.get_voltage,"40":self.get_temperature, "100":self.release, "101":self.unrelease, "110":self.open, "111":self.close}
 
     def handle_control_message(self, request):
-        log_string = "Message recieved at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S")
+        log_string = "Message recieved at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         rospy.loginfo(log_string)
-        print("Request")
-        print("Operation type: %d"%request.operation_type)
-        print("Speed: %d"%request.speed)
-        print("Position %d"%request.position)
+        request_string = "Request:| Operation type: %d | Speed: %d | Position: %d"%(request.operation_type, request.speed, request.position)
+        rospy.loginfo(request_string)
         try:
             self.pseudo_switch['%d'%request.operation_type]()
         except:
-            rospy.loginfo("While operation error occured %s"%datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S"))
+            rospy.loginfo("While operation error occured %s"%datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                     
         return "Operation %d ended"%request.operation_type
 
     def handle_control_message_server(self):
         rospy.init_node('gripper_control_node')
         service = rospy.Service('control_gripper', control, self.handle_control_message)
-        log_string = "service started at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:$S")
+        log_string = "Service started at %s" %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         rospy.loginfo(log_string)
         rospy.spin()
     
